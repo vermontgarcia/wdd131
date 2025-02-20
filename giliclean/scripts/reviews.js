502edfe442034e8ff4bd3e933dc69cab18363701
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-20 16:47:56 -0700

    Reviews

---
+++ giliclean/scripts/reviews.js
@@ -0,0 +1,51 @@
+import { reviews, inputStartAttributes } from "./content.js";
+
+// DOM Selectors
+const servicesSection = document.getElementById("reviews");
+
+console.log(servicesSection);
+
+reviews.forEach(({ user, imgUrl, date, stars, review }) => {
+  // Create Review Cards and add them to the DOM
+  const cardDiv = document.createElement("div");
+
+  const headerDiv = document.createElement("div");
+  const avatarImg = document.createElement("img");
+  avatarImg.setAttribute("src", imgUrl);
+  avatarImg.setAttribute("alt", `${user} - avatar image`);
+
+  const userDataDiv = document.createElement("div");
+  const nameDiv = document.createElement("div");
+  nameDiv.innerHTML = user;
+  const dateDiv = document.createElement("div");
+  // TODO: Calculate time ago
+  dateDiv.innerHTML = date;
+
+  userDataDiv.append(nameDiv, dateDiv);
+
+  headerDiv.append(avatarImg, userDataDiv);
+
+  const starsDiv = document.createElement("div");
+
+  const reviewP = document.createElement("p");
+  reviewP.innerHTML = review;
+
+  cardDiv.append(headerDiv, starsDiv, reviewP);
+
+  inputStartAttributes.forEach(({ type, id, name, value }, index) => {
+    const starInput = document.createElement("input");
+    starInput.setAttribute("type", type);
+    // starInput.setAttribute("id", id);
+    starInput.setAttribute("name", name);
+    starInput.setAttribute("value", value);
+    if (index === stars) {
+      // Selected
+      starInput.setAttribute("selected", true);
+    }
+    starsDiv.appendChild(starInput);
+  });
+
+  servicesSection.appendChild(cardDiv);
+});
+
+export default { reviews, inputStartAttributes };
