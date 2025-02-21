4be677d8634e899374d1e05f779173d2a4808e1d
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-21 08:02:48 -0700

    Join our team home page

--- giliclean/scripts/team.js
+++ giliclean/scripts/team.js
@@ -3,21 +3,23 @@ import { teamMembers } from "./content.js";
 // DOM Selectors Home Page
 const joinOurTeamSection = document.getElementById("join-our-team");
 
-teamMembers.slice(0, 3).forEach(({ imgUrl, name, captions, more }) => {
-  // Create teamMembers Cards and add them to the DOM
-  const cardDiv = document.createElement("div");
-  const teamMemberPicture = document.createElement("img");
-  const captionsP = document.createElement("p");
-  const moreP = document.createElement("p");
+if (joinOurTeamSection) {
+  teamMembers.slice(0, 3).forEach(({ imgUrl, name, captions, more }) => {
+    // Create teamMembers Cards and add them to the DOM
+    const cardDiv = document.createElement("div");
+    const teamMemberPicture = document.createElement("img");
+    const captionsP = document.createElement("p");
+    const moreP = document.createElement("p");
 
-  teamMemberPicture.setAttribute("src", imgUrl);
-  teamMemberPicture.setAttribute("alt", `${name} picture`);
-  captionsP.innerHTML = `<span class="member-name">${name}</span> - <span class="member-captions">${captions}</span>`;
-  moreP.innerHTML = more;
+    teamMemberPicture.setAttribute("src", imgUrl);
+    teamMemberPicture.setAttribute("alt", `${name} picture`);
+    captionsP.innerHTML = `<span class="member-name">${name}</span> - <span class="member-captions">${captions}</span>`;
+    moreP.innerHTML = more;
 
-  cardDiv.append(teamMemberPicture, captionsP, moreP);
+    cardDiv.append(teamMemberPicture, captionsP, moreP);
 
-  joinOurTeamSection.appendChild(cardDiv);
-});
+    joinOurTeamSection.appendChild(cardDiv);
+  });
+}
 
 export default { teamMembers };
