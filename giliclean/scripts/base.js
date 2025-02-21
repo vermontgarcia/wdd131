daaf0139c4324b0eb31c55d9a40a1aa01498de2d
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-21 07:58:40 -0700

    Hamburger menu

--- giliclean/scripts/base.js
+++ giliclean/scripts/base.js
@@ -20,3 +20,18 @@ document.getElementById("current-year").innerHTML = getYear();
 document.getElementById(
   "last-modified"
 ).innerHTML = `Last Modification: ${getLastUpdate()}`;
+
+// Hamburger menu
+const hamButton = document.querySelector("#menu");
+const hamMenuButton = document.querySelector("#ham-menu");
+const largeMenu = document.querySelector("#menu-large");
+const headerTitle = document.querySelector("#header-title");
+const navigation = document.querySelector("#navigation");
+
+hamButton.addEventListener("click", () => {
+  hamButton.classList.toggle("show");
+  hamMenuButton.classList.toggle("show");
+  largeMenu.classList.toggle("show");
+  headerTitle.classList.toggle("show");
+  navigation.classList.toggle("show");
+});
