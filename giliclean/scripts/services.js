06db133bf3029f76928949de1f0107f7c1dcb5c7
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-22 08:29:17 -0700

    Services

--- giliclean/scripts/services.js
+++ giliclean/scripts/services.js
@@ -23,4 +23,29 @@ if (servicesSection) {
   servicesSection.append(cardsContainer);
 }
 
+// DOM Services Page
+
+const servicesAllPageSection = document.querySelector(
+  "#services-all .container"
+);
+
+if (servicesAllPageSection) {
+  const cardsContainer = document.createElement("div");
+  cardsContainer.setAttribute("class", "cards-container");
+  services.forEach(({ name, description }) => {
+    // Create Services Cards and add them to the DOM
+    const cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class", "card");
+    const cardH3 = document.createElement("h3");
+    const cardP = document.createElement("p");
+
+    cardH3.innerHTML = name;
+    cardP.innerHTML = description;
+
+    cardDiv.append(cardH3, cardP);
+
+    cardsContainer.appendChild(cardDiv);
+  });
+  servicesAllPageSection.append(cardsContainer);
+}
 export default { services };
