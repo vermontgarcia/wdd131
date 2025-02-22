bdef4ef0bf0c9efe2a3033f508aa1e9e2915c9bc
Vermont Garcia <vermontgarcia@gmail.com>
2025-02-22 07:21:25 -0700

    About initial settings

---
+++ giliclean/scripts/about.js
@@ -0,0 +1,43 @@
+import {
+  aboutUsSpeach,
+  ourStorySpeach,
+  ourMissionSpeach,
+  ourValues,
+  whyChooseUs,
+} from "./content";
+
+// About Us
+
+const aboutUsContainer = document.getElementById("about-us-container");
+
+if (aboutUsContainer) {
+}
+
+// Our Story
+const ourStoryContainer = document.getElementById("our-story-container");
+
+if (ourStoryContainer) {
+}
+
+// Our Mission
+const ourMissionContainer = document.getElementById("our-mission-container");
+
+if (ourMissionContainer) {
+}
+// Our Values
+const ourValuesContainer = document.getElementById("our-values-container");
+
+if (ourValuesContainer) {
+}
+// Why Chooose Us
+const whyChooseUsContainer = document.getElementById("why-choose-us");
+
+if (whyChooseUsContainer) {
+}
+export default {
+  aboutUsSpeach,
+  ourStorySpeach,
+  ourMissionSpeach,
+  ourValues,
+  whyChooseUs,
+};
